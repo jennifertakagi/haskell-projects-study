5afd2cbf323fe975ad26dd3903e89fe593b0fb80
jennifertakagi <jennitakagi@gmail.com>
2021-03-13 11:32:52 -0300

    feat: add cleanIntList

--- chapter2/Exercises.hs
+++ chapter2/Exercises.hs
@@ -10,3 +10,11 @@ isVowel letter = elem letter "aeiouAEIOU"
 -- Function that receives a string and checks if it is a palindrome
 isPalindrome :: String -> Bool
 isPalindrome word = reverse word == word
+
+-- Function that receives a integer list and remove the numbers: odd, even
+-- multiples by 7 and negative ones. The return is reverse list
+cleanIntList :: [Int] -> [Int]
+cleanIntList intList = (reverse) [ i | i <- intList, (odd) i, (oddNotMultiplySeven) i, (signum) i == 1]
+
+oddNotMultiplySeven :: Int -> Bool
+oddNotMultiplySeven value = (odd) value && (mod) value 7 /= 0
